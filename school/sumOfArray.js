915a185284708e0ddfbc3347020f998c70ce8e4d
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-10 22:27:24 +0530

    [School][Array] sum of array

---
+++ school/sumOfArray.js
@@ -0,0 +1,19 @@
+
+
+/**
+ * @param {number[]} arr
+ * @param {number} n
+ * @return {integer}
+ */
+
+// time: O(n) | space O(1)
+ class Solution {
+    sum(arr,n) {
+        let result = 0;
+        for (let i = 0; i < n; i++) {
+            result += arr[i];
+        }
+       return result;
+    }
+}
+
