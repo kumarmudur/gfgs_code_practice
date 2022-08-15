107422c9adca249fd91dd48dbfc4e3dc176b4c1b
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-15 09:49:55 +0530

    [School][Array] missing number solution 2

--- school/missinNumber.js
+++ school/missinNumber.js
@@ -16,13 +16,12 @@ function missingNumber(arr) {
 // time: O(n) | space: O(1)
 function missingNumber(array,n){
     let total = Math.floor(n * (n + 1) / 2);
-    console.log(total);
     for (let i = 0; i < n - 1; i++) {
-        console.log('inside loop', total);
-        console.log('inside loop', i);
         total -= array[i];
     }   
     return total;
 }
 
 console.log(missingNumber([1,2,3,5], 5));
+
+
