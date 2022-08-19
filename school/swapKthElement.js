b6ee58998598a2f7376ab7c3bc96d2b8cd73468c
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-19 17:53:43 +0530

    [School][Array] swap kth element example solution 2

--- school/swapKthElement.js
+++ school/swapKthElement.js
@@ -8,5 +8,22 @@ function swapKth(arr, n, k) {
     return arr;
 }
 
+// Solution 2
+function swapKth(arr, n, k) {
+    let left = k - 1;
+    let right = n - k;
+    let tmpLeft;
+    let tmpRight;
+    for (let i = 0; i < arr.length; i++) {
+        if (i === left) {
+            tmpLeft = arr[i];
+            arr[i] = tmpRight ? tmpRight : arr[right];
+        } else if (i === right) {
+            tmpRight = arr[i];
+            arr[i] = tmpLeft ? tmpLeft : arr[left];
+        }
+    }
+   return arr;
+}
 
 swapKth([5, 3, 6, 1, 2], 5, 2)
