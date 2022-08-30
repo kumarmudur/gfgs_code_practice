2c87312e3bd0fb36bc9b25aab313614307655d73
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-30 20:51:44 +0530

    [JS][Medium][Arrays] rearrange array elements solution 2

--- medium/arrays/rearragnge.js
+++ medium/arrays/rearragnge.js
@@ -15,4 +15,23 @@ function rearrange(arr, n) {
     return result;
 }
 
+// Solution 2
+// time: O(n) | space: O(1)
+function rearrange(arr, n) {
+    let result = [...arr];
+    let left = 0;
+    let right = n - 1;
+       
+    for (let i = 0; i < n; i++) {
+        if (i % 2 === 0) {
+            arr[i] = result[right];
+            right--;
+        } else {
+            arr[i] = result[left];
+            left++
+        }
+    }
+    return arr;
+}
+
 rearrange([1,2,3,4,5,6], 6);
